c1093b921dfef4e195cc2acf5a7cb523db4428c2
fitriaramadhani20 <fitriramadhanifr99@gmail.com>
2025-09-11 16:41:07 +0700

    datadiri

---
+++ src/modul_1/tipedata/TipeData.java
@@ -0,0 +1,25 @@
+package modul_1.tipedata;
+
+public class TipeData {
+    public static void main(String[] args) {
+        String nama, alamat;
+        int usia;
+        double tinggi;
+        char jk;
+        float beratbadan;
+
+        nama = "Fitri Ramadhani";
+        alamat = "Tambon Tunong";
+        usia = 19;
+        tinggi = 155;
+        jk = "p" ;
+        beratbadan = 42.1f ;
+
+        System.out.println("Nama:" + nama);
+        System.out.println("Alamat:" + alamat);
+        System.out.println("Usia: " + usia);
+        System.out.println("Tinggi: " + tinggi);
+        System.out.println("Jk:" + jk);
+        System.out.println("Beratbadan: " + beratbadan);
+    }
+}
